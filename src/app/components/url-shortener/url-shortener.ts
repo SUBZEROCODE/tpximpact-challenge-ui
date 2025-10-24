2cd9ac475280bf575cb08fd674a2ffa98fb6869d
Sean James <aseanjames2020@gmail.com>
2025-10-24 20:22:19 +0100

    Add in endpoints and url mapping model to bring data into, endpoints all working as expected, just needs a tidy of the UI now

--- src/app/components/url-shortener/url-shortener.ts
+++ src/app/components/url-shortener/url-shortener.ts
@@ -1,29 +1,69 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { UrlShortenerService } from '../../services/url-shortener-service';
+import { UrlMapping } from '../../models/url-mapping.model';
+import { catchError, throwError } from 'rxjs';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-url-shortener',
   templateUrl: './url-shortener.html',
-  styleUrl: './url-shortener.scss'
+  styleUrl: './url-shortener.scss',
+  imports: [CommonModule, RouterModule]
 })
 export class UrlShortenerComponent implements OnInit {
   private urlShortener = inject(UrlShortenerService);
 
   testAliasResponse: string = "";
   apiHealth: string = "";
+  errorMessage: string = "";
+
+  aliasMatchedUrlsReturned: UrlMapping[] = [];
 
   ngOnInit() {
-
     this.urlShortener.getHealthOfAPI().subscribe((healthStatus: string) => {
-      console.log(healthStatus)
       this.apiHealth = healthStatus;
 
-      this.urlShortener.getUrlRedirectForAlias().subscribe((result: string) => {
-          this.testAliasResponse = result;
+      this.urlShortener.getAllAliasedShortenedUrls().subscribe((aliasedShortenedUrls: UrlMapping[]) => {
+        this.aliasMatchedUrlsReturned = aliasedShortenedUrls;
       });
 
-    });
+        // this.urlShortener.getUrlRedirectForAlias("my-custom-alias").subscribe((fullUrlToRedirectTo: string) => {
+        //       // eslint-disable-next-line no-undef
+        //       console.log("Redirecting to " + fullUrlToRedirectTo);
+        //       // window.location.href = fullUrlToRedirectTo;
+        //       // this.testAliasResponse = fullUrlToRedirectTo;
+        // });
 
+        const urlMappingTest: UrlMapping = {
+            alias: "another-custom-alias-v3.1",
+            fullUrl: "https://example.com/very/long/url/v2/even/longer/again",
+            shortUrl: "http://localhost:8080/another-custom-alias"
+        };
+
+        let resultFromSavingMapping: string = this.saveAliasesShortenedUrlMapping(urlMappingTest.fullUrl, urlMappingTest.alias);
+        console.log(resultFromSavingMapping);
+    });
   }
 
+  saveAliasesShortenedUrlMapping(fullUrl: string, alias: string): string {
+    let resultToReturn = "";
+    this.urlShortener.saveAliasedShortenedUrlMapping(fullUrl, alias).pipe(
+        catchError(err => {
+            if (err.status === 400) {
+          this.errorMessage = 'Invalid input. Please check your alias or URL.';
+          } else {
+            this.errorMessage = 'Unexpected error occurred.';
+        }
+        return throwError(() => err);
+      })
+      ).subscribe((result) => {
+        resultToReturn = result;
+    });
+    return resultToReturn;
+  }
+
+  redirectToAlias(alias: string) {
+    this.urlShortener.getUrlRedirectForAlias(alias);
+  }
 }
--- src/app/services/url-shortener-service.ts
+++ src/app/services/url-shortener-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
+import { UrlMapping } from '../models/url-mapping.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,23 @@ export class UrlShortenerService {
   private baseUrl = 'http://localhost:8080/api/v1/url-shortener';
 
   // Get url redirect For a given alias.
-  getUrlRedirectForAlias(): Observable<string> {
-    return this.http.get(`${this.baseUrl}/some-test-alias`, { responseType: 'text' });
+  getUrlRedirectForAlias(customAlias: string): void {
+    window.location.href= `http://localhost:8080/api/v1/url-shortener/${customAlias}`;
   }
 
   getHealthOfAPI(): Observable<string> {
     return this.http.get(`${this.baseUrl}/health`, { responseType: 'text' });
+  }
+
+  getAllAliasedShortenedUrls(): Observable<UrlMapping[]> {
+    return this.http.get<UrlMapping[]>(`${this.baseUrl}/urls`);
+  }
+
+  saveAliasedShortenedUrlMapping(fullUrl: string, customAlias: string): Observable<string> {
+    return this.http.post(`${this.baseUrl}/shorten`, {fullUrl, customAlias}, { responseType: 'text' as const });
+  }
+
+  deleteShortenedUrlMapping(customAlias: string): Observable<string> {
+    return this.http.delete(`${this.baseUrl}/${customAlias}`, { responseType: 'text' });
   }
 }
