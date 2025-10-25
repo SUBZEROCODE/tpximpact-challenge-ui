c21da1c10debb0d59210ead619414d331b110548
Sean James <aseanjames2020@gmail.com>
2025-10-25 17:31:39 +0100

    Add successMessage viewer now we are handling those also as success

---
+++ src/app/components/url-shortener/success-message-viewer/success-message-viewer.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-success-message-viewer',
+  imports: [CommonModule],
+  templateUrl: './success-message-viewer.html',
+  styleUrl: './success-message-viewer.scss',
+})
+export class SuccessMessageViewerComponent implements OnChanges {
+  @Input() successMessage: string = "";
+
+  ngOnChanges(): void {
+    if (this.hasSuccessMessage) {
+      // eslint-disable-next-line no-undef
+      setTimeout(() => this.closeSuccessMessage(), 5000);
+    }
+  }
+
+  get hasSuccessMessage(): boolean {
+    return this.successMessage !== "";
+  }
+  
+  closeSuccessMessage() {
+    this.successMessage = "";
+  }
+}
