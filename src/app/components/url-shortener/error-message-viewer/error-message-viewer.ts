b2246ef52245b04807ec8a9d5dc76106740a57d2
Sean James <aseanjames2020@gmail.com>
2025-10-25 17:26:11 +0100

    Update error message viewer with handling for when api is down

--- src/app/components/url-shortener/error-message-viewer/error-message-viewer.ts
+++ src/app/components/url-shortener/error-message-viewer/error-message-viewer.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-error-message-viewer',
@@ -12,15 +12,27 @@ export class ErrorMessageViewerComponent {
   @Input() errorHandlerInput?: HttpErrorResponse;
   errorMessage = "";
 
+  get hasErrorMessage(): boolean {
+    return this.errorMessage !== "";
+  }
+
+  ngOnChanges(): void {
+      this.errorMessageHandler();
+  }
+
   // Centralized error handler
   errorMessageHandler() {
     if(this.errorHandlerInput){
-      this.errorMessage = this.errorHandlerInput.error?.message || this.errorHandlerInput.statusText
+      if (this.errorHandlerInput.status === 0) {
+        this.errorMessage = "Unable to connect to the API. The backend API may be offline or unreachable."
+      } else {
+          this.errorMessage = this.errorHandlerInput.error?.message || this.errorHandlerInput.error
+      }
     }
 
     // Auto-dismiss after 5 seconds
     // eslint-disable-next-line no-undef
-    setTimeout(() => this.closeErrorMessage(), 5000);
+    setTimeout(() => this.closeErrorMessage(), 10000);
   }
 
   closeErrorMessage() {
