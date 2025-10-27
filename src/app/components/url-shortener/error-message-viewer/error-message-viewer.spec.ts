b35454b4f3f0d034cfe0be794bbafff6d4e9f6c2
Sean James <aseanjames2020@gmail.com>
2025-10-27 09:17:26 +0000

    Code coverage of 70% is generally acceptable, set to 65% for functions coverage, to see if the plugin enforces and fails build run-ft-branch-cicd

--- src/app/components/url-shortener/error-message-viewer/error-message-viewer.spec.ts
+++ src/app/components/url-shortener/error-message-viewer/error-message-viewer.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorMessageViewerComponent } from './error-message-viewer';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('ErrorMessageViewer', () => {
   let component: ErrorMessageViewerComponent;
@@ -27,4 +28,30 @@ describe('ErrorMessageViewer', () => {
       fixture.detectChanges();
       expect(component.errorMessage).toEqual("")
   });
+
+  it('when errorMessageHandler() is called with status 0, set error message that api is offline', ()=> {
+    const apiUnhealthyText = 'Unable to connect to the API. The backend API may be offline or unreachable.';
+    component.errorHandlerInput = new HttpErrorResponse({
+      status: 0,
+      statusText: 'Unknown Error',
+      url: '/api/v1/url-shortener/health',
+      error: { message: 'testErrorMessage' }
+    });
+
+    component.errorMessageHandler();
+    expect(component.errorMessage).toEqual(apiUnhealthyText);
+  });
+
+  it('when errorMessageHandler() is called with another status e.g. 404, set error message to that given errorMessage', ()=> {
+    component.errorHandlerInput = new HttpErrorResponse({
+      status: 404,
+      statusText: 'Not Found',
+      url: `/api/v1/url-shortener/some-test-alias`,
+      error: { message: 'Alias not found' }
+    });
+
+    component.errorMessageHandler();
+    expect(component.errorMessage).toEqual('Alias not found');
+  });
+
 });
